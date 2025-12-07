496fce68bc0f77d354052e044355cdd2f3af66b4
huisamenjohannes5-cmyk <you@example.com>
2025-12-07 22:58:39 +0000

    auto-commit for c7746948-138b-4ad2-8811-bf670a7695de

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import AuthCallback from './pages/AuthCallback';
 import Builder from './pages/Builder';
 import Projects from './pages/Projects';
 import ProjectEditor from './pages/ProjectEditor';
@@ -15,10 +16,11 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/auth/callback" element={<AuthCallback />} />
           <Route path="/" element={<Navigate to="/projects" replace />} />
           <Route path="/projects" element={<ProtectedRoute><Projects /></ProtectedRoute>} />
           <Route path="/builder" element={<ProtectedRoute><Builder /></ProtectedRoute>} />
-          <Route path="/project/:projectId" element={<ProtectedRoute><ProjectEditor /></ProtectedRoute>} />
+          <Route path="/editor/:projectId" element={<ProtectedRoute><ProjectEditor /></ProtectedRoute>} />
         </Routes>
         <Toaster />
       </div>
